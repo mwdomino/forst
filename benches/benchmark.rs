fc85d451f60ece486e27663f6c91bbd3dac0605d
mwdomino <mdominey@equinix.com>
2024-05-11 16:11:33 -0400

    enable history

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,4 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rs_datastore::nestedmap::options::*;
 use rs_datastore::nestedmap::{Item, NestedMap}; // Import your NestedMap module
 
 fn bench_get(c: &mut Criterion) {
@@ -14,11 +15,15 @@ fn bench_get(c: &mut Criterion) {
 }
 
 fn bench_set(c: &mut Criterion) {
-    let mut nm = NestedMap::new(1);
+    let mut nm = NestedMap::new(5);
 
     c.bench_function("set_key", |b| {
         b.iter(|| {
-            nm.set(&"a.b.c.d.e".to_string(), b"some value a", None);
+            nm.set(
+                &"a.b.c.d.e".to_string(),
+                b"some value a",
+                Some(SetOptions::new().preserve_history(true)),
+            );
         });
     });
 }
