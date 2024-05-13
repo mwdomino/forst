63260cbd8f04c897a52c08dae18e39589628e404
mwdomino <mdominey@equinix.com>
2024-05-13 06:15:12 -0400

    add more benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,4 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand::distributions::{Alphanumeric, Distribution};
+use rand::{thread_rng, Rng};
 use rs_datastore::nestedmap::options::*;
 use rs_datastore::nestedmap::{Item, NestedMap}; // Import your NestedMap module
 
@@ -191,10 +193,78 @@ fn seed_queries(nm: &mut NestedMap) {
     );
 }
 
+fn random_key() -> String {
+    let mut rng = thread_rng();
+    let len = rng.gen_range(1..10); // Random length between 1 and 10 for each part
+    (0..5)
+        .map(|_| {
+            Alphanumeric
+                .sample_iter(&mut rng)
+                .take(len)
+                .map(char::from)
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .join(".")
+}
+
+fn bench_set_varying_ttls(c: &mut Criterion) {
+    let mut nm = NestedMap::new(1);
+    seed_100k_keys(&mut nm);
+
+    let key = random_key();
+    let ttl = std::time::Duration::from_secs(thread_rng().gen_range(60..86400)); // TTL between 1 minute and 1 day
+
+    c.bench_function("set with varying TTLs in heap", |b| {
+        b.iter(|| {
+            nm.set(&key, b"this is a value", Some(SetOptions::new().ttl(ttl)));
+        });
+    });
+}
+
+fn bench_set_diverse_keys(c: &mut Criterion) {
+    let mut nm = NestedMap::new(1);
+    seed_100k_keys(&mut nm);
+    let ttl = std::time::Duration::from_secs(thread_rng().gen_range(60..86400)); // TTL between 1 minute and 1 day
+    let key = random_key();
+
+    c.bench_function("set with diverse keys in heap", |b| {
+        b.iter(|| {
+            nm.set(&key, b"this is a value", Some(SetOptions::new().ttl(ttl)));
+        });
+    });
+}
+
+fn seed_100k_keys(nm: &mut NestedMap) {
+    for i in 0..100_000 {
+        let part1 = (i % 100).to_string(); // Cycle through 0 to 99
+        let part2 = ((i / 100) % 100).to_string(); // Cycle through 0 to 99
+        let part3 = (i / 10_000).to_string(); // Cycle through 0 to 9
+
+        let key = format!("{}.{}.{}", part1, part2, part3);
+        let value = b"sample_value";
+
+        let options = Some(SetOptions {
+            ttl: std::time::Duration::from_secs(60 * 5),
+            preserve_history: false,
+        });
+
+        nm.set(&key, value, options);
+    }
+}
+
+//criterion_group!(
+//    benches,
+//    bench_set,
+//    bench_set_diverse_keys,
+//    bench_set_varying_ttls
+//);
 criterion_group!(
     benches,
     bench_get,
     bench_set,
+    bench_set_diverse_keys,
+    bench_set_varying_ttls,
     bench_query_direct,
     bench_query_wildcard,
     bench_query_prefix,
