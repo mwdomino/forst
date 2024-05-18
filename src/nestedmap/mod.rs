498d1bddb71a69e9fc9dadd41e7ea6fdd4e0333c
mwdomino <mdominey@equinix.com>
2024-05-18 12:51:24 -0400

    sort/cleanup

--- src/nestedmap/mod.rs
+++ src/nestedmap/mod.rs
@@ -4,7 +4,7 @@ use std::time::SystemTime;
 
 use serde::{Deserialize, Serialize};
 
-use super::nestedmap::expiration_manager::ExpirationEntry;
+use expiration_manager::ExpirationEntry;
 
 pub mod config;
 pub mod delete;
@@ -23,12 +23,6 @@ pub struct NestedMap {
     id_counter: Arc<AtomicI64>,
 }
 
-#[derive(Debug)]
-pub enum NestedValue {
-    Map(NestedMap),
-    Items(VecDeque<Item>),
-}
-
 #[derive(PartialEq, Debug, Clone, Serialize, Deserialize)]
 pub struct Item {
     pub key: String,
@@ -37,39 +31,10 @@ pub struct Item {
     pub id: i64,
 }
 
-// New
-impl NestedMap {
-    pub fn new(max_history: usize) -> Self {
-        NestedMap {
-            data: BTreeMap::new(),
-            max_history,
-            exp_heap: Mutex::new(BinaryHeap::new()),
-            id_counter: Arc::new(AtomicI64::new(0)),
-        }
-    }
-}
-
-impl NestedMap {
-    pub fn check_expirations(&mut self) {
-        let now = SystemTime::now();
-        let mut heap = self.exp_heap.lock().unwrap();
-
-        // Collect keys to delete
-        let mut keys_to_delete = Vec::new();
-        while let Some(entry) = heap.peek() {
-            if entry.expires_at > now {
-                break;
-            }
-            let entry = heap.pop().unwrap();
-            keys_to_delete.push(entry.keys.clone());
-        }
-        drop(heap); // Explicitly drop heap to release the lock
-
-        // Process deletions
-        for keys in keys_to_delete {
-            self.delete_at_index(&keys, 1); // TODO - use the real index
-        }
-    }
+#[derive(Debug)]
+pub enum NestedValue {
+    Map(NestedMap),
+    Items(VecDeque<Item>),
 }
 
 // Helper function to get mutable reference to nested map if the variant is Map
@@ -78,6 +43,17 @@ impl NestedValue {
         match self {
             NestedValue::Map(map) => &mut map.data,
             _ => panic!("Expected NestedValue to be Map"),
+        }
+    }
+}
+
+impl NestedMap {
+    pub fn new(max_history: usize) -> Self {
+        NestedMap {
+            data: BTreeMap::new(),
+            max_history,
+            exp_heap: Mutex::new(BinaryHeap::new()),
+            id_counter: Arc::new(AtomicI64::new(0)),
         }
     }
 }
