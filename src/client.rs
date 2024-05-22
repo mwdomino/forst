bf2ec13cbbb4c3cff5a320b34edca501e1fcbd2b
Jarrod Baumann <jabaumann@equinix.com>
2024-05-22 00:18:47 -0500

    add optional raw argument for get and query

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,9 @@
 use tonic::transport::Channel;
 use tonic::Request;
 
+use clap::{Arg, ArgAction, Command};
 use rmp_serde::decode::from_read_ref;
 use serde_json::{json, to_string, to_string_pretty, Value};
-use clap::{Arg, Command};
 
 use datastore::datastore_client::DatastoreClient;
 use datastore::{GetRequest, QueryRequest, SetRequest};
@@ -20,26 +20,31 @@ const DEFAULT_PORT: &str = "7777";
 async fn get(
     client: &mut DatastoreClient<Channel>,
     key: String,
+    raw: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let request = GetRequest { key: key.clone() };
     let response = client.get(Request::new(request)).await?;
     let item = response.into_inner();
 
     if let Some(item) = item.item {
-        // deserialize messagepack into serde_json::Value
-        match from_read_ref::<_, Value>(&item.value) {
-            Ok(value) => {
-                if let Ok(json_str) = to_string_pretty(&value) {
-                    println!("{}", json_str);
-                } else {
-                    println!("Error formatting JSON");
+        if raw {
+            println!("{}", general_purpose::STANDARD.encode(&item.value));
+        } else {
+            // deserialize messagepack into serde_json::Value
+            match from_read_ref::<_, Value>(&item.value) {
+                Ok(value) => {
+                    if let Ok(json_str) = to_string_pretty(&value) {
+                        println!("{}", json_str);
+                    } else {
+                        println!("Error formatting JSON");
+                    }
                 }
-            }
-            Err(e) => {
-                println!(
-                    "[key: {}] Failed to deserialize MessagePack data: {:?}",
-                    item.key, e
-                );
+                Err(e) => {
+                    println!(
+                        "[key: {}] Failed to deserialize MessagePack data: {:?}",
+                        item.key, e
+                    );
+                }
             }
         }
     } else {
@@ -76,28 +81,36 @@ async fn query(
     client: &mut DatastoreClient<Channel>,
     key: String,
     history_count: Option<i64>,
+    raw: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let options = history_count.map(|count| datastore::GetOptions {
         history_count: Some(count),
     });
 
-    let request = QueryRequest {key, options};
+    let request = QueryRequest { key, options };
     let response = client.query(Request::new(request)).await?;
 
     let items = response.into_inner().items;
     let mut results = Vec::new();
 
     for item in items {
-        // deserialize messagepack into serde_json::Value
-        let value = match from_read_ref::<_, Value>(&item.value) {
-            Ok(value) => value,
-            Err(_) => json!({"error": "Failed to deserialize MessagePack data"}),
-        };
+        if raw {
+            results.push(json!({
+                "key": item.key,
+                "value": general_purpose::STANDARD.encode(&item.value)
+            }));
+        } else {
+            // deserialize messagepack into serde_json::Value
+            let value = match from_read_ref::<_, Value>(&item.value) {
+                Ok(value) => value,
+                Err(_) => json!({"error": "Failed to deserialize MessagePack data"}),
+            };
 
-        results.push(json!({
-            "key": item.key,
-            "value": value
-        }));
+            results.push(json!({
+                "key": item.key,
+                "value": value
+            }));
+        }
     }
 
     // serialize results as json and return!
@@ -117,18 +130,44 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .author("Sr. Rust Developer")
         .arg_required_else_help(true)
         .subcommand_required(true)
-        .subcommand(Command::new("get")
-            .about("gets a value for a key")
-            .arg(Arg::new("key").required(true)))
-        .subcommand(Command::new("set")
-            .about("sets a value for a key with optional ttl")
-            .arg(Arg::new("key").required(true))
-            .arg(Arg::new("value").required(true))
-            .arg(Arg::new("ttl").required(false).value_parser(clap::value_parser!(i64))))
-        .subcommand(Command::new("query")
-            .about("queries a key with optional history count")
-            .arg(Arg::new("key").required(true))
-            .arg(Arg::new("history_count").required(false).value_parser(clap::value_parser!(i64))))
+        .subcommand(
+            Command::new("get")
+                .about("gets a value for a key")
+                .arg(Arg::new("key").required(true))
+                .arg(
+                    Arg::new("raw")
+                        .long("raw")
+                        .action(ArgAction::SetTrue)
+                        .help("returns raw data"),
+                ),
+        )
+        .subcommand(
+            Command::new("set")
+                .about("sets a value for a key with optional ttl")
+                .arg(Arg::new("key").required(true))
+                .arg(Arg::new("value").required(true))
+                .arg(
+                    Arg::new("ttl")
+                        .required(false)
+                        .value_parser(clap::value_parser!(i64)),
+                ),
+        )
+        .subcommand(
+            Command::new("query")
+                .about("queries a key with optional history count")
+                .arg(Arg::new("key").required(true))
+                .arg(
+                    Arg::new("history_count")
+                        .required(false)
+                        .value_parser(clap::value_parser!(i64)),
+                )
+                .arg(
+                    Arg::new("raw")
+                        .long("raw")
+                        .action(ArgAction::SetTrue)
+                        .help("returns raw data"),
+                ),
+        )
         .get_matches();
 
     // Retrieve host and port from environment or use default values
@@ -141,22 +180,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     match cmd.subcommand() {
         Some(("get", sub_matches)) => {
             let key = sub_matches.get_one::<String>("key").unwrap();
-            get(&mut client, key.to_string()).await?;
-        },
+            let raw = sub_matches.get_flag("raw");
+
+            get(&mut client, key.to_string(), raw).await?;
+        }
         Some(("set", sub_matches)) => {
             let key = sub_matches.get_one::<String>("key").unwrap();
-            let value = sub_matches.get_one::<String>("value").unwrap().as_bytes().to_vec();
-            let ttl = sub_matches.get_one::<String>("ttl")
-                        .and_then(|v| v.parse::<i64>().ok())
-                        .unwrap_or(0);
+            let value = sub_matches
+                .get_one::<String>("value")
+                .unwrap()
+                .as_bytes()
+                .to_vec();
+            let ttl = sub_matches
+                .get_one::<String>("ttl")
+                .and_then(|v| v.parse::<i64>().ok())
+                .unwrap_or(0);
             set(&mut client, key.to_string(), value, ttl).await?;
-        },
+        }
         Some(("query", sub_matches)) => {
             let key = sub_matches.get_one::<String>("key").unwrap();
-            let history_count = sub_matches.get_one::<String>("history_count")
-                                  .and_then(|v| v.parse::<i64>().ok());
-            query(&mut client, key.to_string(), history_count).await?;
-        },
+            let history_count = sub_matches
+                .get_one::<String>("history_count")
+                .and_then(|v| v.parse::<i64>().ok());
+            let raw = sub_matches.get_flag("raw");
+
+            query(&mut client, key.to_string(), history_count, raw).await?;
+        }
         _ => unreachable!(),
     }
 
