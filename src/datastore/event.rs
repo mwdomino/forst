777334c0011cf1ae66ee38a7751527a55a1414e9
mwdomino <mdominey@equinix.com>
2024-05-23 14:45:28 -0400

    fix timer, improve logging
    If you want to see the info level logs you can run:
    RUST_LOG=info cargo test
    
    (or just export RUST_LOG=info)

--- src/datastore/event.rs
+++ src/datastore/event.rs
@@ -1,3 +1,5 @@
+use log::{info};
+
 use std::time::Duration;
 use std::time::SystemTime;
 
@@ -20,7 +22,7 @@ impl Datastore {
         let ttl = self.ttl.clone();
         let sender = self.event_sender.clone();
 
-        println!("Starting event loop");
+        info!("Starting event loop");
 
         tokio::spawn(async move {
             let timer = tokio::time::sleep(Duration::from_secs(u64::MAX));
@@ -29,26 +31,25 @@ impl Datastore {
             loop {
                 tokio::select! {
                     event = receiver.recv() => {
-                        println!("Received event: {:?}", event);
+                        info!("Received event: {:?}", event);
 
                         if let Some(event) = event {
                             match event {
                                 Event::TTLInsert(entry) => {
                                     let mut ttl_guard = ttl.lock().await;
-                                    println!("Inserted entry: key:{} id:{}", entry.key, entry.id);
+                                    info!("Inserted entry: key:{} id:{}", entry.key, entry.id);
+                                    let now = SystemTime::now();
 
                                     if let Some(next_expiry) = ttl_guard.peek() {
-                                        println!("there was an entry in ttl already");
+                                        info!("there was an entry in ttl already");
                                         if next_expiry.expires_at > entry.expires_at {
-                                            let now = SystemTime::now();
                                             let duration = entry.expires_at.duration_since(now).unwrap_or(Duration::new(0, 0));
 
                                             timer.as_mut().reset(time::Instant::now() + duration);
-                                            println!("Old timer updated using key:{} id:{}!", entry.key, entry.id);
+                                            info!("Old timer updated using key:{} id:{}!", entry.key, entry.id);
                                         }
                                     } else {
-                                        println!("No ttl entry found, inserting new");
-                                        let now = SystemTime::now();
+                                        info!("No ttl entry found, inserting new");
                                         let duration = entry.expires_at.duration_since(now).unwrap_or(Duration::new(0, 0));
 
                                         timer.as_mut().reset(time::Instant::now() + duration);
@@ -60,7 +61,7 @@ impl Datastore {
                                     let mut map_guard = map.lock().await;
                                     map_guard.delete_by_id(&entry.key, entry.id);
 
-                                    println!("Deleted entry: key:{} id:{}", entry.key, entry.id);
+                                    info!("Deleted entry: key:{} id:{}", entry.key, entry.id);
 
                                     // need to update the timer now
                                     let ttl_guard = ttl.lock().await;
@@ -74,7 +75,7 @@ impl Datastore {
                                     }
                                 },
                                 Event::Notify => {
-                                    println!("Notify event received");
+                                    info!("Notify event received");
                                 }
                             }
                         } else {
@@ -82,13 +83,19 @@ impl Datastore {
                         }
                     },
                     () = &mut timer => {
-                    //_ = timer.as_mut().unwrap(), if timer.is_some() => {
                         // Timer expired, process expiration
                         let mut ttl_guard = ttl.lock().await;
-                        if let Some(min_entry) = ttl_guard.pop() {
-                            println!("Timer expired for key:{} id:{}", min_entry.key, min_entry.id);
 
-                            let _ = sender.send(Event::TTLExpired(min_entry)).await;
+                        if let Some(next_expiry) = ttl_guard.peek() {
+                            // ensure entry is expired
+                            if next_expiry.expires_at < SystemTime::now() {
+                                if let Some(min_entry) = ttl_guard.pop() {
+                                    info!("Timer expired for key:{} id:{}", min_entry.key, min_entry.id);
+
+                                    timer.as_mut().reset(time::Instant::now() + Duration::from_secs(5000));
+                                    let _ = sender.send(Event::TTLExpired(min_entry)).await;
+                                }
+                            }
                         }
                     }
                 }
